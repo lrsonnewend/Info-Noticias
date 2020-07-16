edf14a89ca80406c641446d62c645104aa3ea206
lucas <lucas.lr184@gmail.com>
2020-07-15 23:06:15 -0300

    inserindo dados em script.sql

--- script.sql
+++ script.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE bdnews;
 
+USE bdnews;
+
 CREATE TABLE usuarios(
     id INT PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR NOT NULL,
@@ -14,3 +16,13 @@ CREATE TABLE noticias(
     data_criacao VARCHAR NOT NULL,
     link_img VARCHAR
 );
+
+INSERT INTO usuarios(email, senha, isAdmin) 
+values("email@teste.com", 1234, true);
+
+INSERT INTO usuarios(email, senha, isAdmin)
+values("usuario@email.com", 1234, false);
+
+INSERT INTO noticias(corpo, titulo, data_criacao, link_img)
+values("O governo liberou a verba para auxílio de pessoas desempregadas para poderem ter uma renda extra para as coisas básicas: alimentos e pagar contas.", "07/04/2020", "https://www.blogdovestibular.com/wp-content/uploads/2020/05/auxilio-emergencial-caixa-economica-federal-2020-1024x572.jpg", "Governo libera auxílio emergencial de R$600,00 reais.");
+
