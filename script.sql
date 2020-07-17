ab2dc439c2cfc7672f4f6ee153db4f6173150b11
lucas <lucas.lr184@gmail.com>
2020-07-16 21:36:03 -0300

    alterando arquivo script.sql

--- script.sql
+++ script.sql
@@ -1,28 +1,110 @@
-CREATE DATABASE bdnews;
+-- MySQL dump 10.13  Distrib 5.7.30, for Linux (x86_64)
+--
+-- Host: localhost    Database: bdnews
+-- ------------------------------------------------------
+-- Server version	5.7.30-0ubuntu0.18.04.1
 
-USE bdnews;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE usuarios(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR NOT NULL,
-    senha VARCHAR NOT NULL,
-    isAdmin BOOLEAN NOT NULL
-);
+--
+-- Current Database: `bdnews`
+--
 
-CREATE TABLE noticias(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    corpo VARCHAR NOT NULL,
-    titulo VARCHAR NOT NULL,
-    data_criacao VARCHAR NOT NULL,
-    link_img VARCHAR
-);
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `bdnews` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-INSERT INTO usuarios(email, senha, isAdmin) 
-values("email@teste.com", 1234, true);
+USE `bdnews`;
 
-INSERT INTO usuarios(email, senha, isAdmin)
-values("usuario@email.com", 1234, false);
+--
+-- Table structure for table `hibernate_sequence`
+--
 
-INSERT INTO noticias(corpo, titulo, data_criacao, link_img)
-values("O governo liberou a verba para auxílio de pessoas desempregadas para poderem ter uma renda extra para as coisas básicas: alimentos e pagar contas.", "07/04/2020", "https://www.blogdovestibular.com/wp-content/uploads/2020/05/auxilio-emergencial-caixa-economica-federal-2020-1024x572.jpg", "Governo libera auxílio emergencial de R$600,00 reais.");
+DROP TABLE IF EXISTS `hibernate_sequence`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `hibernate_sequence` (
+  `next_val` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `hibernate_sequence`
+--
+
+LOCK TABLES `hibernate_sequence` WRITE;
+/*!40000 ALTER TABLE `hibernate_sequence` DISABLE KEYS */;
+INSERT INTO `hibernate_sequence` VALUES (20);
+/*!40000 ALTER TABLE `hibernate_sequence` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `noticias`
+--
+
+DROP TABLE IF EXISTS `noticias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `noticias` (
+  `id` int(11) NOT NULL,
+  `corpo` varchar(255) NOT NULL,
+  `data_criacao` varchar(255) NOT NULL,
+  `link_img` varchar(255) DEFAULT NULL,
+  `titulo` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `noticias`
+--
+
+LOCK TABLES `noticias` WRITE;
+/*!40000 ALTER TABLE `noticias` DISABLE KEYS */;
+INSERT INTO `noticias` VALUES (5,'O governo liberou a verba para auxílio de pessoas desempregadas para poderem ter uma renda extra para as coisas básicas: alimentos e pagar contas.','07/04/2020','https://www.blogdovestibular.com/wp-content/uploads/2020/05/auxilio-emergencial-caixa-economica-federal-2020-1024x572.jpg','Governo libera auxílio emergencial de R$600,00 reais.'),(14,'Mais de 4000 casos foram diagnosticados na cidade neste último mês de Julho, e os comércios estão voltando a abrir normalmente.','02/07/2020','https://valenews.com.br/wp-content/uploads/2020/04/PonteEstaiadaSJC23042020ClaudioVieira.jpg','COVID-19 cresce na cidade de São José dos Campos'),(19,'inserindo uma nova notícia de apresentação','20/06/2020','https://www.welivesecurity.com/wp-content/uploads/2019/10/resumen-mensual-noticias-seguridad-inform%C3%A1tica-623x423.jpg','Teste de nova notícia');
+/*!40000 ALTER TABLE `noticias` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `isAdmin` bit(1) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'email@teste.com',_binary '','1234'),(5,'lucas@email.com',_binary '\0','1234'),(9,'rodrigo@email.com',_binary '\0','1234');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-07-16 21:04:33
